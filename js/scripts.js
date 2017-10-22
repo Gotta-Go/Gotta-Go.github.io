098eaa5492009a3940f173e17c22fd83509e6dcd
KieranMoynihan <kieranamoynihan@gmail.com>
2017-10-22 03:27:47 -0400

    javascript and jquery bleh

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,18 @@ function current_color(new_color){
     var cur = document.getElementById("current");
     cur.style.color = new_color;
 }
+
+$(function(){
+	var currentPos = 0;
+	$(window).scroll(function(event){
+		var newPos = $(this).scrollTop();
+		
+		if(newPos > currentPos){
+			$("header").slideUp("fast");
+		}
+		if(newPos < currentPos){
+			$("header").slideDown("fast");
+		}
+		currentPos = newPos;
+	});
+});
